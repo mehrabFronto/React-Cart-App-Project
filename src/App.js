05d440d7d35422496f4bd80cb1f0b7c19f33164f
MehrabD20 <mehrab20@gmail.com>
2023-05-06 22:59:40 +0430

    cart context+reducer added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import "./App.css";
 import Layout from "./Layout/Layout";
 import { Route, Switch } from "react-router-dom";
 import { routes } from "./routes";
+import CartProvider from "./Providers/CartProvider";
 
 const App = () => {
    return (
       <div className="app">
-         <Layout>
-            <Switch>
-               {routes.map((route) => {
-                  return (
-                     <Route
-                        key={route.path}
-                        {...route}
-                     />
-                  );
-               })}
-            </Switch>
-         </Layout>
+         <CartProvider>
+            <Layout>
+               <Switch>
+                  {routes.map((route) => {
+                     return (
+                        <Route
+                           key={route.path}
+                           {...route}
+                        />
+                     );
+                  })}
+               </Switch>
+            </Layout>
+         </CartProvider>
       </div>
    );
 };
