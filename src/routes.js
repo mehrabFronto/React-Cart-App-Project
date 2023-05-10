217cf47e8f4393f680d488633f2981085194fb0f
MehrabD20 <mehrab20@gmail.com>
2023-05-10 21:16:53 +0430

    signup and login pages added

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import CartPage from "./pages/CartPage/CartPage";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
 import HomePage from "./pages/HomePage/HomePage";
+import LogInPage from "./pages/LogInPage/LogInPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 export const routes = [
    { path: "/cart", component: CartPage },
    { path: "/checkout", component: CheckoutPage },
+   { path: "/sign-up", component: SignUpPage },
+   { path: "/log-in", component: LogInPage },
+   { path: "/cart", component: CartPage },
    { path: "/", component: HomePage, exact: true },
 ];
